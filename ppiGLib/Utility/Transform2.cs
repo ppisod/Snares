da5c411b186bb7105dcaace50364bb654c6c53c8
ppi <hicnkebromzih@gmail.com>
2025-10-24 14:17:08 +0800

    Delete ppiGLib directory

--- ppiGLib/Utility/Transform2.cs
+++
@@ -1,8 +0,0 @@
-namespace ppiGLib.Utility;
-
-public class Transform2 (Stretch2 pos, Stretch2 size, float rot)
-{
-    public Stretch2 Position { get; set; } = pos;
-    public Stretch2 Size { get; set; } = size;
-    public float Rotation { get; set; } = rot;
-}
