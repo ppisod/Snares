6e277962b2e0f494cd2f4d922fab8a9fa753ea75
ppisod <hicnkebromzih@gmail.com>
2025-11-04 05:42:09 +0800

    BeatmapSelectorScreen.cs workspace.xml

--- Snares/Game/Screens/BeatmapSelectorScreen.cs
+++ Snares/Game/Screens/BeatmapSelectorScreen.cs
@@ -1,18 +1,46 @@
+using System.Drawing;
 using ppilib.Input;
 using ppilib.Interfaces;
+using ppilib.Node.Custom;
+using ppilib.Utility.Configs;
+using ppilib.Utility.MovingThings.Ease.Definitions;
+using Color = Microsoft.Xna.Framework.Color;
+using Vector2 = Microsoft.Xna.Framework.Vector2;
 
 namespace Snares.Game.Screens;
 
 public enum BeatmapSelectorContext
 {
-    
+    None, Game, BackToTitle
 }
 
-public class BeatmapSelectorScreen(Game1 game, INode parent, MouseController mouse, KeyboardController keyboard)
+public partial class BeatmapSelectorScreen(Game1 game, INode parent, MouseController mouse, KeyboardController keyboard)
     : Screen<BeatmapSelectorContext>(game, parent, mouse, keyboard)
 {
     protected override void Initialize()
     {
+        Context = BeatmapSelectorContext.None;
+        var nodeConfig = new NodeConfig(null, Game.GraphicsDevice, true, true, true, true, true);
+
+        nodeConfig
+            .SetParent(Parent)
+            .SetLerpMethod(EasingTypes.Quad.EaseOut)
+            .SetColor(Color.Black * 0.6f)
+            .SetOpacity(0f);
         
+        // NODE :: Back button
+        nodeConfig
+            .SetPos(new Vector2(0, 0.01f)).SetScale(new Vector2(0.2f, 0.1f))
+            .SetName("BackButton")
+            .SetText("BACK");
+
+        var back = new ContinuousTextFrame(nodeConfig);
+        Parent.AddChild(back);
+        
+        
+        // NODEGROUP :: buttons
+        
+
     }
+    
 }
