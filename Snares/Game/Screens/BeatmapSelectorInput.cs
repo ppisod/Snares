30c9aab2d4ab01340aba452e5e64952bc6d6e430
ppisod <hicnkebromzih@gmail.com>
2025-11-04 05:51:14 +0800

    BeatmapSelectorInput.cs workspace.xml

--- Snares/Game/Screens/BeatmapSelectorInput.cs
+++ Snares/Game/Screens/BeatmapSelectorInput.cs
@@ -1,6 +1,32 @@
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using ppilib.Node.Custom;
+using Snares.Game.Screens.Effects;
+
 namespace Snares.Game.Screens;
 
 public partial class BeatmapSelectorScreen
 {
-    
+    private class ScaleSelector : IContinuousTweenSelector<ContinuousTextFrame, Vector2> // move these to other files?
+    {
+        public ppilib.Utility.MovingThings.Interfaces.IContinuousTween<Vector2>? Select(ContinuousTextFrame item) => item.Scale;
+    }
+
+    private void UpdatePointer(MouseState state)
+    {
+        var buttons = NodeGroups.TryGetValue("Buttons", out var buttonNodes)
+            ? buttonNodes.Cast<ContinuousTextFrame>().ToArray()
+            : [];
+        
+        PointerContinuousTweener.Apply(
+            buttons,
+            n => n.GetRect().Contains(state.Position),
+            new ScaleSelector(),
+            rest: new Vector2(0.2f, 0.1f),
+            hover: new Vector2(0.22f, 0.12f),
+            pressed: new Vector2(0.19f, 0.09f),
+            mouse: state
+        );
+    }
 }
